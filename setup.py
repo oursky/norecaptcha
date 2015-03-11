18919000223e6174b9755ff7b4c59b49433f5ad7
Rick Mak <rick.mak@gmail.com>
2015-03-11 17:39:14 +0800

    Bump version to 0.2, add classsifiers for py2 only

--- setup.py
+++ setup.py
@@ -6,12 +6,23 @@ README = path.abspath(path.join(path.dirname(__file__), 'README.md'))
 
 setup(
    name='norecaptcha',
-   version='0.1.1',
+   version='0.2',
    packages=['norecaptcha'],
    description='Python client for the google new No CAPTCHA reCAPTCHA services.',
    long_description=open(README).read(),
    author='Oursky Ltd.',
    author_email='rick.mak@gmail.com',
    url='https://github.com/oursky/norecaptcha',
-   license='MIT'
+   download_url='https://www.github.com/oursky/norecaptcha/tarball/master',
+   license='MIT',
+   classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
